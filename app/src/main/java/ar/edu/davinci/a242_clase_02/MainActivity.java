227b8ced42f56be40b70e09194e8d0fba78ba6c3
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-08-20 22:23:16 -0300

    feat: add log onCreate

--- app/src/main/java/ar/edu/davinci/a242_clase_02/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a242_clase_02/MainActivity.java
@@ -3,12 +3,16 @@ package ar.edu.davinci.a242_clase_02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "activity-filter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "Hello Worlds!!");
     }
 }
